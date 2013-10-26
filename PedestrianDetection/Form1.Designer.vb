ed3152f7fc0e2fbefb8628a33926fccee2c4383a
MicrocontrollersAndMore <cdahms123@gmail.com>
2013-10-26 13:27:57 -0400

    commit

---
+++ PedestrianDetection/Form1.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+		Me.lblFile = New System.Windows.Forms.Label()
+		Me.txtFile = New System.Windows.Forms.TextBox()
+		Me.btnFile = New System.Windows.Forms.Button()
+		Me.ibImage = New Emgu.CV.UI.ImageBox()
+		Me.ofdFile = New System.Windows.Forms.OpenFileDialog()
+		CType(Me.ibImage,System.ComponentModel.ISupportInitialize).BeginInit
+		Me.SuspendLayout
+		'
+		'lblFile
+		'
+		Me.lblFile.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.lblFile.Location = New System.Drawing.Point(4, 4)
+		Me.lblFile.Name = "lblFile"
+		Me.lblFile.Size = New System.Drawing.Size(100, 28)
+		Me.lblFile.TabIndex = 0
+		Me.lblFile.Text = "choose file:"
+		Me.lblFile.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+		'
+		'txtFile
+		'
+		Me.txtFile.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.txtFile.Location = New System.Drawing.Point(108, 4)
+		Me.txtFile.Name = "txtFile"
+		Me.txtFile.Size = New System.Drawing.Size(648, 27)
+		Me.txtFile.TabIndex = 1
+		'
+		'btnFile
+		'
+		Me.btnFile.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.btnFile.Location = New System.Drawing.Point(756, 4)
+		Me.btnFile.Name = "btnFile"
+		Me.btnFile.Size = New System.Drawing.Size(31, 28)
+		Me.btnFile.TabIndex = 2
+		Me.btnFile.Text = "..."
+		Me.btnFile.UseVisualStyleBackColor = true
+		'
+		'ibImage
+		'
+		Me.ibImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.ibImage.Location = New System.Drawing.Point(2, 36)
+		Me.ibImage.Name = "ibImage"
+		Me.ibImage.Size = New System.Drawing.Size(786, 432)
+		Me.ibImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+		Me.ibImage.TabIndex = 2
+		Me.ibImage.TabStop = false
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(8!, 16!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(791, 471)
+		Me.Controls.Add(Me.ibImage)
+		Me.Controls.Add(Me.btnFile)
+		Me.Controls.Add(Me.txtFile)
+		Me.Controls.Add(Me.lblFile)
+		Me.Name = "Form1"
+		Me.Text = "Pedestrian Detection Example"
+		CType(Me.ibImage,System.ComponentModel.ISupportInitialize).EndInit
+		Me.ResumeLayout(false)
+		Me.PerformLayout
+
+End Sub
+    Friend WithEvents lblFile As System.Windows.Forms.Label
+    Friend WithEvents txtFile As System.Windows.Forms.TextBox
+    Friend WithEvents btnFile As System.Windows.Forms.Button
+    Friend WithEvents ibImage As Emgu.CV.UI.ImageBox
+    Friend WithEvents ofdFile As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ PedestrianDetection/Form1.vb
@@ -0,0 +1,84 @@
+﻿Option Strict
+
+Imports Emgu.CV
+Imports Emgu.CV.CvEnum
+Imports Emgu.CV.Structure
+Imports Emgu.CV.UI
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Public Class Form1
+
+' member variables ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Dim blnFirstTimeInResizeEvent As Boolean = True
+Dim intOrigFormWidth As Integer = 0
+Dim intOrigFormHeight As Integer = 0
+Dim intOrigImageBoxWidth As Integer = 0
+Dim intOrigImageBoxHeight As Integer = 0
+
+' constructor '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Sub New()
+	InitializeComponent					'always call this in a visual basic form constructor, necessary to init form
+
+	intOrigFormWidth = Me.Width
+	intOrigFormHeight = Me.Height
+	intOrigImageBoxWidth = ibImage.Width
+	intOrigImageBoxHeight = ibImage.Height
+End Sub
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Private Sub Form1_Resize( sender As System.Object,  e As System.EventArgs) Handles MyBase.Resize
+	'This If Else statement is necessary to throw out the first time the Form1_Resize event is called.
+	'For some reason, in VB.NET the Resize event is called once before the constructor, then the constructor is called,
+	'then the Resize event is called each time the form is resized.  The first time the Resize event is called
+	'(i.e. before the constructor is called) the coordinates of the components on the form all read zero,
+	'therefore we have to throw out this first call, then the constructor will run and get the correct initial
+	'component location data, then every time after that we can let the Resize event run as expected
+	If(blnFirstTimeInResizeEvent = True) Then
+		blnFirstTimeInResizeEvent = False
+	Else
+		ibImage.Width = Me.Width - (intOrigFormWidth - intOrigImageBoxWidth)
+		ibImage.Height = Me.Height - (intOrigFormHeight - intOrigImageBoxHeight)
+	End If
+End Sub
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Private Sub btnFile_Click( sender As System.Object,  e As System.EventArgs) Handles btnFile.Click
+	Dim drDialogResult As DialogResult = ofdFile.ShowDialog()
+
+	If(drDialogResult = Windows.Forms.DialogResult.OK Or drDialogResult = Windows.Forms.DialogResult.Yes) Then
+		txtFile.Text = ofdFile.FileName
+	End If
+End Sub
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Private Sub txtFile_TextChanged( sender As System.Object,  e As System.EventArgs) Handles txtFile.TextChanged
+	If(txtFile.Text <> String.Empty) Then
+		ProcessImageAndUpdateGUI
+	End If
+End Sub
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Sub ProcessImageAndUpdateGUI()
+	Dim imgImage As Image(Of Bgr, Byte) = New Image(Of Bgr, Byte)(txtFile.Text)
+	Dim imgBlank As Image(Of Bgr, Byte) = Nothing
+	Dim hogd As HOGDescriptor = New HOGDescriptor()
+	Dim rectPedestrians() As Rectangle
+
+	Me.Text = "processing, please wait . . ."
+	ibImage.Image = imgBlank
+	Application.DoEvents()
+
+	hogd.SetSVMDetector(HOGDescriptor.GetDefaultPeopleDetector())
+	rectPedestrians = hogd.DetectMultiScale(imgImage)
+
+	For Each rectPedestrian As Rectangle In rectPedestrians
+		imgImage.Draw(rectPedestrian, New Bgr(Color.Red), 2)
+	Next
+	
+	ibImage.Image = imgImage
+
+	Me.Text = "done processing, choose another image if desired"
+
+End Sub
+
+End Class
